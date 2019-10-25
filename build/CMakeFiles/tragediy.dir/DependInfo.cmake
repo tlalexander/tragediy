202dac065e320187e78df269e676355c06cbbe1e
Taylor Alexander <tlalexander@gmail.com>
2019-10-25 10:27:49 -0700

    Added generated tracks as well as build files.

---
+++ build/CMakeFiles/tragediy.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/taylor/Software/tragediy/src/track/AnkiCommon.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/AnkiCommon.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/AnkiDriveMap.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/AnkiDriveMap.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/AnkiOverdriveMap.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/AnkiOverdriveMap.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/Lane.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/Lane.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/LaneArcTile.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/LaneArcTile.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/LaneLineTile.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/LaneLineTile.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/LaneTileBase.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/LaneTileBase.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/LocationTable.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/LocationTable.cpp.o"
+  "/home/taylor/Software/tragediy/src/track/Track.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/track/Track.cpp.o"
+  "/home/taylor/Software/tragediy/src/tragediy.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/tragediy.cpp.o"
+  "/home/taylor/Software/tragediy/src/util/AssertionError.cpp" "/home/taylor/Software/tragediy/build/CMakeFiles/tragediy.dir/src/util/AssertionError.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../system"
+  "inc"
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tragediy.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tragediy.dir/src/track/AnkiDriveMap.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/AnkiOverdriveMap.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/AnkiCommon.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/Lane.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/LaneArcTile.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/LaneLineTile.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/LaneTileBase.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/LocationTable.cpp.o"
+  "CMakeFiles/tragediy.dir/src/track/Track.cpp.o"
+  "CMakeFiles/tragediy.dir/src/util/AssertionError.cpp.o"
+  "CMakeFiles/tragediy.dir/src/tragediy.cpp.o"
+  "tragediy.pdb"
+  "tragediy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tragediy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
